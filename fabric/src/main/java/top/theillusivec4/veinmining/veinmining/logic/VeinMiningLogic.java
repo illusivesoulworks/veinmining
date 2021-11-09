7a5b6a72400031fe86867aea5a945bda2ff3a794
TheIllusiveC4 <29991504+TheIllusiveC4@users.noreply.github.com>
2021-11-08 23:20:01 -0800

    fix prevent tool destruction logic

--- fabric/src/main/java/top/theillusivec4/veinmining/veinmining/logic/VeinMiningLogic.java
+++ fabric/src/main/java/top/theillusivec4/veinmining/veinmining/logic/VeinMiningLogic.java
@@ -163,7 +163,7 @@ public class VeinMiningLogic {
         if (gameMode != GameMode.CREATIVE) {
           ItemStack itemStack = player.getMainHandStack();
           ItemStack itemStack2 = itemStack.copy();
-          boolean bl2 = player.isUsingEffectiveTool(blockState);
+          boolean bl2 = player.canHarvest(blockState);
 
           if (VeinMiningConfig.VeinMining.addToolDamage) {
             postMine(itemStack, world, blockState, pos, player);
@@ -186,7 +186,7 @@ public class VeinMiningLogic {
       int damage = VeinMiningConfig.VeinMining.toolDamageMultiplier;
 
       if (VeinMiningConfig.VeinMining.preventToolDestruction) {
-        damage = Math.min(damage, stack.getMaxDamage() - stack.getDamage() - 1);
+        damage = Math.min(damage, stack.getMaxDamage() - stack.getDamage() - 2);
       }
 
       if (damage > 0) {
